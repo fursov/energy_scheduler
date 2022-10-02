a576f5d4f29535c0a1add70973c83192936ed904
Dmitry Fursov <dfoursov@yahoo.com>
2022-10-02 12:52:24 +0300

    Added low tax rate period in Finland

--- nordpool_prices.py
+++ nordpool_prices.py
@@ -8,12 +8,17 @@ from nordpool import elspot
 
 logger = logging.getLogger(__name__)
 
-FI_VAT = 1.24
 FI_TIMEZONE = pytz.timezone('Europe/Helsinki')
+FI_LOW_TAX_RATE_START = datetime.datetime(2023, 1, 1, 0, 0, 0, tzinfo=FI_TIMEZONE)
+FI_LOW_TAX_RATE_END = datetime.datetime(2023, 4, 30, 23, 59, 59, tzinfo=FI_TIMEZONE)
+FI_REGULAR_VAT = 1.24
+FI_LOW_VAT = 1.10
 
 
 def get_prices():
     prices_spot = elspot.Prices()
+    now = datetime.datetime.now()
+    tax_rate = FI_REGULAR_VAT if now < FI_LOW_TAX_RATE_START or now > FI_LOW_TAX_RATE_END else FI_LOW_VAT
     try:
         prices_spot_finland_today = prices_spot.hourly(end_date=datetime.date.today(), areas=['FI'])
     except ConnectionError:
@@ -21,7 +26,7 @@ def get_prices():
         return []
     prices = [{
             'time': data['start'].astimezone(FI_TIMEZONE),
-            'value': round(data['value'] / 10.0 * FI_VAT, 2),
+            'value': round(data['value'] / 10.0 * tax_rate, 2),
         } for data in prices_spot_finland_today['areas']['FI']['values'] if data['value'] != float('inf')]
     try:
         prices_spot_finland_tomorrow = \
@@ -31,7 +36,7 @@ def get_prices():
         return prices
     prices.extend([{
             'time': data['start'].astimezone(FI_TIMEZONE),
-            'value': round(data['value'] / 10.0 * FI_VAT, 2),
+            'value': round(data['value'] / 10.0 * tax_rate, 2),
         } for data in prices_spot_finland_tomorrow['areas']['FI']['values'] if data['value'] != float('inf')])
     return prices
 
