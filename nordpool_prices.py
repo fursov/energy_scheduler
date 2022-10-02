ab147ba35cd80168160fa27ba4daeeb5fe224562
Dmitry Fursov <dfoursov@yahoo.com>
2022-10-02 12:42:30 +0300

    Fixed nordpool results if tomorrow's values are not availabe

--- nordpool_prices.py
+++ nordpool_prices.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 
 import datetime
+import logging
 import pytz
 
 from nordpool import elspot
 
+logger = logging.getLogger(__name__)
 
 FI_VAT = 1.24
 FI_TIMEZONE = pytz.timezone('Europe/Helsinki')
@@ -12,17 +14,25 @@ FI_TIMEZONE = pytz.timezone('Europe/Helsinki')
 
 def get_prices():
     prices_spot = elspot.Prices()
-    prices_spot_finland_today = prices_spot.hourly(end_date=datetime.date.today(), areas=['FI'])
+    try:
+        prices_spot_finland_today = prices_spot.hourly(end_date=datetime.date.today(), areas=['FI'])
+    except ConnectionError:
+        logger.warning('Could not fetch nordpool today\'s prices!')
+        return []
     prices = [{
             'time': data['start'].astimezone(FI_TIMEZONE),
             'value': round(data['value'] / 10.0 * FI_VAT, 2),
-        } for data in prices_spot_finland_today['areas']['FI']['values']]
-    prices_spot_finland_tomorrow = \
-        prices_spot.hourly(end_date=datetime.date.today() + datetime.timedelta(days=1), areas=['FI'])
+        } for data in prices_spot_finland_today['areas']['FI']['values'] if data['value'] != float('inf')]
+    try:
+        prices_spot_finland_tomorrow = \
+            prices_spot.hourly(end_date=datetime.date.today() + datetime.timedelta(days=1), areas=['FI'])
+    except ConnectionError:
+        logger.warning('Could not fetch nordpool tomorrow\'s prices!')
+        return prices
     prices.extend([{
             'time': data['start'].astimezone(FI_TIMEZONE),
             'value': round(data['value'] / 10.0 * FI_VAT, 2),
-        } for data in prices_spot_finland_tomorrow['areas']['FI']['values']])
+        } for data in prices_spot_finland_tomorrow['areas']['FI']['values'] if data['value'] != float('inf')])
     return prices
 
 
